b5b5e87f9cb64822678150fec176a3adbefa0603
sakinkirti <sakin.arya.kirti@gmail.com>
2022-03-19 14:32:54 -0400

    semantics.py file working now

--- semantics.py
+++ semantics.py
@@ -4,23 +4,24 @@
 
 # import packages
 import sys
-import pandas as pd
+import pandas
 from nrclex import NRCLex
 
-# called when script is called
-def main():
-    print(getSemantics(sys.argv[1]))
-
-# to gather semantic data
+# does all the work
 def getSemantics(text):
     data = NRCLex(text)
     top_emotions = data.top_emotions
 
-    emotions_wo_score = []
-    # get only the emotions without the value score
-    for emotion,i in enumerate(top_emotions):
-        if i == 3:
-            return emotions_wo_score
-        
-        # append the next emotion
-        emotions_wo_score.append(top_emotions[i][0])
+    # get only the emotions without the value score and return the top 3 emotions
+    emotions_wo_score = [emotion[0] for emotion in top_emotions]
+    #return emotions_wo_score
+
+    if len(emotions_wo_score) > 3:
+        return emotions_wo_score[0:3]
+    else:
+        return emotions_wo_score
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    for arg in sys.argv:
+        print(getSemantics(arg))
