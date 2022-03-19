0302f7ab834f9d679bd3dad491d0bd093caf5dde
sakinkirti <sakin.arya.kirti@gmail.com>
2022-03-19 14:18:28 -0400

    jupyter notebook for testing and script that partially works - getting it to work with terminal now

--- semantics.py
+++ semantics.py
@@ -3,28 +3,24 @@
 # to gather semantics on the journal entry
 
 # import packages
-import nltk
-import nrclex
+import sys
+import pandas as pd
 from nrclex import NRCLex
 
+# called when script is called
 def main():
-    
+    print(getSemantics(sys.argv[1]))
 
 # to gather semantic data
 def getSemantics(text):
     data = NRCLex(text)
     top_emotions = data.top_emotions
 
-    emotions_without_score = []
+    emotions_wo_score = []
     # get only the emotions without the value score
     for emotion,i in enumerate(top_emotions):
         if i == 3:
-            return emotions_without_score
+            return emotions_wo_score
         
         # append the next emotion
-        emotions_without_score.append(top_emotions[i][0])
-
-
-# to control the main method execution
-if __name__ == '__name__':
-    main()
+        emotions_wo_score.append(top_emotions[i][0])
