ccf43cefe3f45291e7f87eb7e0a297d2a64ff172
andrew0144 <80224229+andrew0144@users.noreply.github.com>
2022-03-26 18:32:37 -0400

    deleted comment

--- textProcesser.js
+++ textProcesser.js
@@ -1,6 +1,5 @@
 function processEntry(){
    $('#summernote').summernote('disable'); // disable editing
   var summernoteText= $(".note-editable p").prepend(" ").text(); // add spaces
-   //var summernoteText = $('.note-editable').text(); // get text
     console.log(summernoteText);
 };
