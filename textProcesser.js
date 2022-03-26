f84f8f60648d0b9410386995c570d004fc9d236d
andrew0144 <80224229+andrew0144@users.noreply.github.com>
2022-03-26 18:31:51 -0400

    processEntry now works and gets text correctly

--- textProcesser.js
+++ textProcesser.js
@@ -1,4 +1,6 @@
 function processEntry(){
-    var summernoteText = $('.note-editable').text(); //get just the text
+   $('#summernote').summernote('disable'); // disable editing
+  var summernoteText= $(".note-editable p").prepend(" ").text(); // add spaces
+   //var summernoteText = $('.note-editable').text(); // get text
     console.log(summernoteText);
 };
